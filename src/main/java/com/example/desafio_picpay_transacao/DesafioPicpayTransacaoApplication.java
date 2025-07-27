faea8dd3690975b7cdd96369835afad94d43cb27
Diego Nascimento <nascimentodln22@gmail.com>
2025-07-26 21:24:14 -0300

    first commit

---
+++ src/main/java/com/example/desafio_picpay_transacao/DesafioPicpayTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_picpay_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpayTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpayTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_picpay_transacao/DesafioPicpayTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_picpay_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
